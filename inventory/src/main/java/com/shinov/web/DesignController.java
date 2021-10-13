3757dd2fef17f71ac805c404217c77e58ec8581a
Vitya <Vitya@host.docker.internal>
2021-10-14 00:58:56 +0300

    data base data can be written into html table

--- inventory/src/main/java/com/shinov/web/DesignController.java
+++ inventory/src/main/java/com/shinov/web/DesignController.java
@@ -8,11 +8,13 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.WebRequest;
 
 import com.shinov.Inventory;
 import com.shinov.Item;
@@ -25,27 +27,29 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
-@RequestMapping(value = "/design", method = RequestMethod.POST)
+@RequestMapping(value = "/design")
 public class DesignController {
-	@GetMapping
-	public String showDesignForm(Model model, @RequestParam(value = "button") String s) {
-		System.out.println(s);
-		doPost(s);
-
-//		ItemsDAO itemsDAO = new ItemsDAO();
-//		
-//		List<Item> items = itemsDAO.findAll();
-//		
+	@GetMapping 
+	public String showDesignForm(Model model, @ModelAttribute(value = "button") String s) {
+	
+		doGet(s);
+	
+		ItemsDAO itemsDAO = new ItemsDAO();
+		
+		List<Item> items = itemsDAO.findAll();
+		
 //		Type[] types = Item.Type.values();
 //		
 //		for (Type type : types) {
 //			model.addAttribute(type.toString().toLowerCase(), filterByType(items, type));
 //		}
 //		model.addAttribute("design", new Inventory());
+		model.addAttribute("items", items);
+		
 		return "design";
 	}
 
-	private void doPost(String s) {
+	private void doGet(String s) {
 		
 		if (s.equals("Generate inventory"))
 			generateInventory();
